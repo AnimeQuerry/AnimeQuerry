ef1b3b2a6399dbf09eb9211561e08cbf78893742
MoisesJPG <148658757+MoisesJPG@users.noreply.github.com>
2024-02-13 22:48:20 +0100

    Summary

--- index.js
+++ index.js
@@ -1,47 +1,8 @@
-var nameDatabase = {
-    "BNHA": 1,
-    "Boku No Hero Academia": 1,
-    "Boku No Hero Academia 1": 1,
-    "Boku No Hero Academia 1st Seasson": 1,
-
-    "BNHA2": 2,
-    "Boku No Hero Academia 2": 2,
-    "Boku No Hero Academia 2nd Seasson": 2,
-
-    "BNHA3": 3,
-    "Boku No Hero Academia 3": 3,
-    "Boku No Hero Academia 3rd Seasson": 3,
-    
-    "Akuyaku Reijou Level 99: Watashi wa Ura-Boss desu ga Maou dewa Arimasen": 4,
-    "Villainess Level 99: I May Be the Hidden Boss but I'm Not the Demon Lord": 4,
-    
-    "Isekai de Mofumofu Nadenade suru Tame ni Ganbattemasu": 5,
-    "I'm Doing My Best to Make Myself at Home in Another World, Mofunade": 5,
-
-    "": -1
-}
-var idDatabase = {
-    1: [
-        "Boku no Hero Academia",
-        "AnimeFLV: https://www3.animeflv.net/anime/boku-no-hero-academia-2016"
-    ],
-    2: [ 
-        "Boku no Hero Academia 2nd Seasson",
-        "AnimeFLV: https://www3.animeflv.net/anime/boku-no-hero-academia-2nd-season"
-    ],
-    3: [
-        "Boku no Hero Academia 3rd Seasson",
-        "AnimeFLV: https://www3.animeflv.net/anime/boku-no-hero-academia-3rd-season"
-    ],
-    4:[
-        "Villainess Level 99: I May Be the Hidden Boss but I'm Not the Demon Lord",
-        "AnimeFLV: https://www3.animeflv.net/anime/akuyaku-reijou-level-99-watashi-wa-uraboss-desu-ga-maou-dewa-arimasen"
-    ],
-    5:[
-        "Isekai de Mofumofu Nadenade suru Tame ni Ganbattemasu.",
-        "AnimeFLV: https://www3.animeflv.net/anime/isekai-de-mofumofu-nadenade-suru-tame-ni-ganbattemasu"
-    ]
-}
+$(document).ready(function() {
+    $.getJSON('index.json', function(data) {
+        console.log(data);
+    })
+});
 function FindAssignedName(id){
     return idDatabase[id][0];
 }
